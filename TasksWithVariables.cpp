de7028c16572259729c9586ef4a2e6bff4a9185b
KacperD123 <113014114+KacperD123@users.noreply.github.com>
2022-10-04 08:56:50 +0200

    Add files via upload

--- TasksWithVariables.cpp
+++ TasksWithVariables.cpp
@@ -1,20 +1,34 @@
 // PROJEKT 2 KACPER DYK.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// s
 
 #include <iostream>
 
-int main()
+void task1()
 {
-    std::cout << "Hello World!\n";
+    int firstnumber;
+    int secondNumber;
+    std::cout << "Podaj pierwsza liczbe:\n";
+    std::cin >> firstnumber;
+    std::cout << "Podaj druga liczbe:\n";
+    std::cin >> secondNumber;
+
+    //suma,roznica       iloczyn ,iloraz
+    int sum, difference, product, quotient;
+    sum = firstnumber + secondNumber;
+    difference = firstnumber - secondNumber;
+    product = firstnumber * secondNumber;
+    quotient = firstnumber / secondNumber;
+
+
+    std::cout << "Suma: " << sum << "\n";
+    std::cout << "roznica: " << difference << "\n";
+    std::cout << "Iloczyn: " << product << "\n";
+    std::cout << "Iloraz:  " << quotient << "\n";
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main()
+{
+    task1();
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
