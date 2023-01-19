27235ee9221f46c2917d0f6f0d706305d6cbb764
Michal <fdsgs>
2023-01-19 14:56:08 +0100

    kkk

---
+++ Zadanie/Zadanie.cpp
@@ -0,0 +1,269 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+
+
+/*
+Zadanie 1
+Napisz funkcjê, która pobierze od u¿ytkownika liczbê dodatni¹ (do skutku).
+*/
+
+void task1()
+{
+    int number;
+
+    do
+    {
+        std::cout << "Podaj liczbê dodatni¹:\n";
+        std::cin >> number;
+    } while (number <= 0);
+
+    std::cout << "Poda³eœ liczbê " << number << "\n";
+}
+
+/*
+Zadanie 2
+Napisz funkcjê, która wyœwietli wszystkie liczby ca³kowite z przedzia³u <1;10>
+*/
+
+void task2()
+{
+    std::cout << "1, 2, 3, 4, 5, 6, 7, 8, 9, 10, \n";
+
+    int currentNumber = 1;
+
+    do
+    {
+        std::cout << currentNumber << ", ";
+        //currentNumber = currentNumber + 1;
+        //currentNumber += 1;
+        currentNumber++;
+    } while (currentNumber != 11); // (currentNumber < 11)
+}
+
+/*
+Zadanie 3
+Napisz funkcjê, która wyœwietli wszystkie liczby ca³kowite z przedzia³u pobranego od u¿ytkownika
+*/
+
+void task3()
+{
+    int lowRange, highRange;
+    std::cout << "Podaj zakres dolny\n";
+    std::cin >> lowRange;
+    std::cout << "Podaj górny zakres\n";
+    std::cin >> highRange;
+
+    int currentNumber = lowRange;
+
+    do
+    {
+        std::cout << currentNumber << ", ";
+        currentNumber++;
+    } while (currentNumber != highRange + 1); // (currentNumber <= highRange)
+}
+
+/*
+Zadanie 4
+Zgadnij liczbê.
+*/
+
+void task4()
+{
+    int randomNumber;
+
+    srand(time(NULL));
+
+    /*
+    <a;b> a <= b
+    <0; b-a>
+    rand() % (b - a + 1) + a;
+    */
+    //od 1 do 100
+    randomNumber = rand() % 100 + 1;
+
+    //std::cout << "Wylosowana liczba to " << randomNumber << "\n";
+
+    int numberFromUser;
+    do
+    {
+        std::cout << "Podaj liczbê:\n";
+        std::cin >> numberFromUser;
+
+        if (numberFromUser > randomNumber)
+            std::cout << "Liczba za du¿a\n";
+        if (numberFromUser < randomNumber)
+            std::cout << "Liczba za ma³a\n";
+    } while (numberFromUser != randomNumber);
+
+    std::cout << "Gratulacje.";
+}
+
+/*
+Zadanie 5
+Napisz funkcjê, która wyœwietli wszystkie liczby ca³kowite z przedzia³u pobranego od u¿ytkownika
+Poprawka do zadania 3 gdzie górny zakres nie mo¿e byæ mniejszy od dolnego.
+*/
+void task5()
+{
+    int lowRange, highRange;
+    std::cout << "Podaj zakres dolny\n";
+    std::cin >> lowRange;
+    std::cout << "Podaj górny zakres\n";
+    std::cin >> highRange;
+
+    if (lowRange > highRange)
+        std::cout << "B³êdny zakres\n";
+
+    int currentNumber = lowRange;
+    while (currentNumber <= highRange)
+    {
+        std::cout << currentNumber << ", ";
+        currentNumber++;
+    }
+    std::cout << "\nTa linijka jest poza petl¹\n";
+}
+
+/*
+Zadanie 6
+Napisz funkcjê która policzy NWD dwóch liczb. NWD - najwiêkszy wspólny dzielnik
+*/
+void task6()
+{
+    int firstNumber, secondNumber;
+    std::cout << "Podaj pierwsz¹ liczbê\n";
+    std::cin >> firstNumber;
+    std::cout << "Podaj drug¹ liczbê\n";
+    std::cin >> secondNumber;
+
+    int dzielnik;
+    if (firstNumber > secondNumber)
+        dzielnik = secondNumber;
+    else
+        dzielnik = firstNumber;
+    //algorytm NWD
+    while (firstNumber % dzielnik > 0 || secondNumber % dzielnik > 0)
+        dzielnik--;
+
+    //Wersja 2 algorytmu
+    /*
+        NWD(a, b) = a jeœli b = 0
+        NWD(a, b) = NWD(b, a % b) jeœli b != 0
+    */
+    int a = firstNumber;
+    int b = secondNumber;
+
+    while (b != 0)
+    {
+        int tmpA = a;
+        a = b;
+        b = tmpA % b;
+    }
+
+    std::cout << "NWD = " << dzielnik << "\n";
+    std::cout << "NWD = " << a << "\n";
+}
+
+/*
+Zadanie 7
+1 => 1 = 1!
+2 => 2 = 1*2 = 2!
+3 => 6 = 1*2*3 = 3!
+4 => 24 = 1*2*3*4 = 4!
+5 => 120 = 1*2*3*4*5 = 5!
+6 => 720 = 1*2*3*4*5*6 = 6!
+7 => 5040 = 1*2*3*4*5*6*7 = 7!
+8 => 5040 * 8 = 1*2*3*4*5*6*7*8 = 8!
+9 => 9!
+Napisz funkcjê, która policzy wartoœæ silni liczby od u¿ytkownika
+*/
+void task7()
+{
+    int numberFromUser;
+    std::cout << "Podaj liczbê:\n";
+    std::cin >> numberFromUser;
+
+    unsigned long long result = 1;
+
+    /*int i = 2;
+    while (i <= numberFromUser)
+    {
+        result = result * i;
+        i++;
+    }*/
+    //1       2                 4
+    for (int i = 2; i <= numberFromUser; i++)
+    {
+        //3
+        result = result * i;
+    }
+
+    std::cout << numberFromUser << "! = " << result << "\n";
+}
+
+/*
+Zadanie 8
+Wczytaæ napis i wyœwietliæ ile jest liter 'a' w tym wyrazie.
+*/
+void task8()
+{
+    std::string textFromUser;
+    std::cout << "Podaj wyraz\n";
+    std::cin >> textFromUser;
+
+    //textFromUser[0] = 'A';
+    std::cout << "U¿ytkownik poda³: " << textFromUser << "\n";
+    std::cout << "Napis sk³ada siê z " << textFromUser.length() << " znaków\n";
+    std::cout << "Pierwszy znak z napisu to " << textFromUser[0] << "\n";   
+    std::cout << "Ostatni znak z napisu to " << textFromUser[textFromUser.length() - 1] << "\n";
+
+    int coutChar = 0;
+
+    for (int i = 0; i < textFromUser.length(); i++)
+    {
+        if (textFromUser[i] == 'a')
+            coutChar++;
+    }
+
+    //liczymy znaki 'a'
+
+    std::cout << "Znaków 'a' jest " << coutChar << "\n";
+}
+void task9()
+{
+    std::string textFromUser;
+    std::cout << "Podaj wyraz\n";
+    std::cin >> textFromUser;
+
+    for (int i = 0; i < textFromUser.length(); i++)
+}
+
+void task10()
+{
+    int numberFromUser;
+
+    cout << "Podaj liczbe\n";
+    cin >> numberFromUser;
+    
+    
+     
+}
+
+int main()
+{
+    setlocale(LC_CTYPE, "polish");
+
+    //task1();
+    //task2();
+    //task3();
+    //task4();
+    //task5();
+    //task6();
+    //task7();
+    //task8();
+    //task9();
+    task10();
+
+}
---
+++ decisionInstrutcion/decisionInstrutcion.cpp
@@ -0,0 +1,243 @@
+﻿#include <iostream>
+
+/*
+DRY - don't repeat yourself - nie powtarzaj się
+*/
+
+/*
+Operatory porównania:
+* równości				==
+* różności				!=
+* większości			>
+* mniejszości			<
+* większe lub równe		>=
+* mniejsze lub równe	<=
+argument1 >= argument2
+Wynikiem działania tych operatorów
+jest wartość logiczna true/false typu bool
+bool zmienna = true;
+bool zmienna2 = 5 > 74;
+*/
+
+/*
+Napisz funkcję, któa wczyta dwie liczby i wykona dzielenie na nich
+*/
+void task1()
+{
+	int firstNumber, secondNumber;
+
+	std::cout << "Podaj pierwsza liczbe:\n";
+	std::cin >> firstNumber;
+
+	std::cout << "Podaj druga liczbe (nie podawaj zera):\n";
+	std::cin >> secondNumber;
+
+	//iloraz
+	int quotient = 0;
+
+	/*if (secondNumber != 0)
+		quotient = firstNumber / secondNumber;
+	if (secondNumber != 0)
+		std::cout << "Iloraz: " << quotient << "\n";
+	if (secondNumber == 0)
+		std::cout << "Dzielenie przez zero!!!!\n";*/
+
+	if (secondNumber != 0)
+	{
+		quotient = firstNumber / secondNumber;
+		std::cout << "Iloraz: " << quotient << "\n";
+	}
+	else
+	{
+		std::cout << "Dzielenie przez zero!!!!\n";
+	}
+
+	std::cout << "Koniec programu\n";
+}
+
+/*
+Napisz funkjcę, która wczyta liczbę i wyświetli informację
+czy jest ona dodatnia czy nie.
+*/
+void task2()
+{
+	int numberToCheck;
+
+	std::cout << "Podaj liczbe do sprawdzenia:\n";
+	std::cin >> numberToCheck;
+
+	if (numberToCheck > 0)
+		std::cout << "Liczba większa od zera\n";
+	else
+		std::cout << "Liczba równa lub mniejsza od zera\n";
+}
+
+/*
+Napisz funkcję, która wczyta liczbę i wyświetli informację
+czy jest ona w zakresie <10, 25>.
+*/
+void task3()
+{
+	int numberToCheck;
+
+	std::cout << "Podaj liczbe do sprawdzenia:\n";
+	std::cin >> numberToCheck;
+	if (numberToCheck >= 10)
+	{
+		std::cout << "Liczba jest w zakresie\n";
+
+		if (numberToCheck <= 25)
+		{
+			std::cout << "Liczba jest w zakresie\n";
+		}
+		else
+		{
+			std::cout << "Liczba nie jest w zakresie\n";
+
+		}
+	}
+	else
+	{
+
+		std::cout << "Liczba nie jest w zakresie\n";
+		
+	}
+
+
+}
+
+/*Sprawdź czy liczba jest parzysta*/
+void task4()
+{
+	int Pierwsza;
+
+	std::cout << "Podaj liczbe\n";
+	std::cin >> Pierwsza;
+
+	if (Pierwsza % 2 == 0)
+		std::cout << "Jest parzysta\n";
+	else
+		std::cout << "Nie jest parzysta\n";
+}
+/*Napisz funkcję która wczyta liczbę która będzie bokiem kwadratu i wyświetli informacje o polu i obwodzie*/
+void task5()
+{
+	int bok, wynik;
+
+	std::cout << "Podaj liczbe\n";
+	std::cin >> bok;
+
+	if (bok * bok)
+		std::cout << "Pole kwadratu wynosi\n";
+	wynik = bok * bok;
+
+}
+void task6()
+{
+	int firstNumber, secondNumber;
+
+		std::cout << "Podaj pierwszą liczbe\n";
+		std::cin >> firstNumber;
+
+		std::cout << "Podaj druga liczbe\n";
+		std::cin >> secondNumber;
+		
+		if (firstNumber > secondNumber)
+			std::cout << "Pierwsza jest wieksza\n";
+		else if (firstNumber < secondNumber)
+			std::cout << "Druga jest wieksza\n";
+		else
+			std::cout << "Liczby sa rowne\n";
+}
+void task7()
+{
+	int firstNumber,secondNumber,threeNumber;
+
+	std::cout << "Podaj pierwszą liczbe\n";
+	std::cin >> firstNumber;
+
+	std::cout << "Podaj druga liczbe\n";
+	std::cin >> secondNumber;
+	
+	std::cout << "Podaj trzecia liczbe\n";
+	std::cin >> threeNumber;
+
+	if (firstNumber > secondNumber && firstNumber > threeNumber)
+		std::cout << "Pierwsza jest najwieksza\n";
+	else if (secondNumber > threeNumber && secondNumber > firstNumber)
+		std::cout << "Druga jest najwieksza\n";
+	else
+		std::cout << "Trzecia jest najwieksza\n";
+
+
+
+
+}
+
+void task10()
+switch (daynumber)
+{
+		case 1:
+			std::cout << "Poniedziałek\n";
+			break;
+		case 2:
+			std::cout << "Wtorek\n";
+			break;
+		case 3:
+			std::cout << "Środa\n";
+			break;
+		case 4:
+			std::cout << "Czwartek\n";
+			break;
+		case 5:
+			std::cout << "Piątek\n";
+			break;
+		case 6:
+			std::cout << "Sobota\n";
+			break;
+		case 0:
+			
+		case 7:
+			std::cout << "Niedziela\n";
+			break;
+		default
+			std::cout << "błedny dzien\n";
+
+
+
+
+
+
+}
+
+void task 11();
+	std::cout <<""
+
+
+/*
+Zadanie 7
+Napisz funkcję która wczyta trzy liczby i wyświetli największą z nich.
+
+Zadanie 8
+Napisz funkcję, która wczyta dwie liczby i wyświetli je w kolejności rosnącej.
+Zadanie 9
+Napisz funkcję, która wczyta trzy liczby i wyświetli je w kolejności rosnącej.
+Zadanie 10
+Napisz funkcję, która wczyta numer dnia tygodnia i wyświetli nazwę tego dnia.
+Zadanie 11 *   
+Napisz funkcję, która wczyta współczynniki równania kwadratowego i wyświetli pierwiastki tego równania*/
+int main()
+
+//task1();
+//task2();
+//task3();
+//task4();
+//task5();
+//task6();
+//task7();
+//task8();
+//task9();
+//task10();
+task11();
+
+}
